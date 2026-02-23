ec1562920e6746ea2e1eb8d4a2156f0b05ce5727
Gia Bao <etsofia69@protonmail.com>
2026-02-23 21:38:41 +0700

    fix path

--- server.js
+++ server.js
@@ -162,7 +162,7 @@ const app = express();
 
 app.use(cors({
     origin: isProduction
-        ? "https://giabao6769.github.io/"
+        ? "https://giabao6769.github.io"
         : "http://127.0.0.1:5500",
     credentials: true
 }));
